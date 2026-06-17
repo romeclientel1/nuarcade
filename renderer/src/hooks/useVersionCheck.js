41cec477f3b81e5d38b55c4255141a99be36addf
romeclientel1 <jerome_emanuel@yahoo.com>
2026-06-17 12:09:37 -0400

    bump version to 4.3.0

--- renderer/src/hooks/useVersionCheck.js
+++ renderer/src/hooks/useVersionCheck.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 
-const CURRENT_VERSION = "4.2.0"
+const CURRENT_VERSION = "4.3.0"
 const RELEASES_API = "https://api.github.com/repos/romeclientel1/nuarcade/releases/latest"
 
 export function useVersionCheck() {
