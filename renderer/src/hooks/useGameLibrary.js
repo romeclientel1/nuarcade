bc78a64c1f7068677cc0f2755fbdd76b49a003c6
romeclientel1 <jerome_emanuel@yahoo.com>
2026-06-19 15:10:45 -0400

    Update useGameLibrary.js

--- renderer/src/hooks/useGameLibrary.js
+++ renderer/src/hooks/useGameLibrary.js
@@ -1,88 +1,7 @@
 import { useState, useEffect } from 'react'
 import { fetchScreenScraperArtwork } from './useScreenScraper'
 
-const SAMPLE_GAMES = [
-  // TeknoParrot
-  { id: 'wanganmd',             title: 'Wangan Midnight MT 5DX+',     genre: 'Racing',   system: 'SEGA Nu',        status: 'Perfect', profile: 'WanganMidnightMaximumTune5DX.xml',       icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'wanganmr',             title: 'Wangan Midnight MT 6RR',      genre: 'Racing',   system: 'SEGA Nu',        status: 'Perfect', profile: 'WanganMidnightMaximumTune6RR.xml',       icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'CruisnBlast',          title: "Cruis'n Blast",               genre: 'Racing',   system: 'Raw Thrills',    status: 'Perfect', profile: 'CruisnBlast.xml',                        icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'FZeroAX',              title: 'F-Zero AX',                   genre: 'Racing',   system: 'Sega Triforce',  status: 'Perfect', profile: 'FZeroAX.xml',                            icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'Daytona3',             title: 'Daytona Championship USA',    genre: 'Racing',   system: 'SEGA PC',        status: 'Perfect', profile: 'DaytonaChampionshipUSA.xml',             icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'AliensArmageddon',     title: 'Aliens Armageddon',           genre: 'Shooter',  system: 'Raw Thrills',    status: 'Perfect', profile: 'AliensArmageddon.xml',                   icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'DariusBurst',          title: 'Dariusburst Another Chron.', genre: 'Shooter',  system: 'Taito Type X2',  status: 'Perfect', profile: 'DariusBurstAnotherChronicle.xml',        icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'CrossbeatsRev',        title: 'Crossbeats Rev Sunrise',      genre: 'Rhythm',   system: 'SEGA Nu',        status: 'Perfect', profile: 'crossbeatsREVSUNRISE.xml',               icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'BlazBlueCrossTag',     title: 'BlazBlue Cross Tag Battle',   genre: 'Fighting', system: 'NESiCAxLive',    status: 'Perfect', profile: 'BlazBlueCrossTagBattle.xml',             icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'BlazBlueCS2',          title: 'BlazBlue Continuum Shift 2', genre: 'Fighting', system: 'NESiCAxLive',    status: 'Perfect', profile: 'BlazBlueContinuumShift2.xml',            icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'HotD4',                title: 'House of the Dead 4',         genre: 'Shooter',  system: 'SEGA Lindbergh', status: 'Perfect', profile: 'HouseOfTheDead4Special.xml',             icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'DOA5',                 title: 'Dead or Alive 5 Ultimate',    genre: 'Fighting', system: 'SEGA RingEdge2', status: 'Perfect', profile: 'DeadOrAlive5UltimateArcade.xml',         icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'AfterBurner',          title: 'After Burner Climax',         genre: 'Flying',   system: 'SEGA Lindbergh', status: 'Perfect', profile: 'AfterBurnerClimax.xml',                  icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'TimeCrisis5',          title: 'Time Crisis 5',               genre: 'Shooter',  system: 'Namco 369',      status: 'Great',   profile: 'TimeCrisis5.xml',                        icon: 'ARC',  emulator: 'teknoparrot' },
-  { id: 'DragonBallZenkai',     title: 'Dragon Ball Zenkai BR',       genre: 'Fighting', system: 'Namco 369',      status: 'Perfect', profile: 'DragonBallZenkaiBattleRoyale.xml',       icon: 'ARC',  emulator: 'teknoparrot' },
-  // RPCS3 (PS3 arcade titles)
-  { id: 'ps3_tekken6',          title: 'Tekken 6',                    genre: 'PS3',      system: 'RPCS3',          status: 'Perfect', profile: 'BLES00494',                              icon: 'PS3',  emulator: 'rpcs3' },
-  { id: 'ps3_blazblue',         title: 'BlazBlue Calamity Trigger',   genre: 'PS3',      system: 'RPCS3',          status: 'Perfect', profile: 'BLES00535',                              icon: 'PS3',  emulator: 'rpcs3' },
-  { id: 'ps3_sf4',              title: 'Street Fighter IV',           genre: 'PS3',      system: 'RPCS3',          status: 'Great',   profile: 'BLES00374',                              icon: 'PS3',  emulator: 'rpcs3' },
-  // Nintendo Switch (Ryujinx)
-  { id: 'switch_ZeldaTotK',     title: 'Zelda: Tears of the Kingdom', genre: 'Switch',   system: 'Nintendo Switch', status:'Perfect', profile: 'ZeldaTotK.nsp',                          icon: 'NSW',  emulator: 'ryujinx' },
-  { id: 'switch_MarioKart8',    title: 'Mario Kart 8 Deluxe',         genre: 'Racing',   system: 'Nintendo Switch', status:'Perfect', profile: 'MarioKart8.nsp',                         icon: 'NSW',  emulator: 'ryujinx' },
-  { id: 'switch_SSBU',          title: 'Super Smash Bros. Ultimate',   genre: 'Fighting', system: 'Nintendo Switch', status:'Perfect', profile: 'SSBU.nsp',                               icon: 'NSW',  emulator: 'ryujinx' },
-  // Xbox 360 (Xenia)
-  { id: 'xbox360_Halo3',        title: 'Halo 3',                      genre: 'Xbox360',  system: 'Xbox 360',        status:'Perfect', profile: 'Halo3.iso',                              icon: 'X36',  emulator: 'xenia' },
-  { id: 'xbox360_Gears',        title: 'Gears of War',                genre: 'Xbox360',  system: 'Xbox 360',        status:'Great',   profile: 'GearsOfWar.iso',                         icon: 'X36',  emulator: 'xenia' },
-  { id: 'xbox360_Forza3',       title: 'Forza Motorsport 3',          genre: 'Racing',   system: 'Xbox 360',        status:'Playable',profile: 'ForzaMotorsport3.iso',                   icon: 'X36',  emulator: 'xenia' },
-  // GameCube / Wii (Dolphin)
-  { id: 'gcwii_MarioKartWii',   title: 'Mario Kart Wii',              genre: 'GCWii',    system: 'Nintendo Wii',    status:'Perfect', profile: 'MarioKartWii.wbfs',                      icon: 'GCN',  emulator: 'dolphin' },
-  { id: 'gcwii_SSBM',           title: 'Super Smash Bros. Melee',     genre: 'Fighting', system: 'GameCube',        status:'Perfect', profile: 'SSBM.iso',                               icon: 'GCN',  emulator: 'dolphin' },
-  { id: 'gcwii_FZeroGX',        title: 'F-Zero GX',                   genre: 'Racing',   system: 'GameCube',        status:'Perfect', profile: 'FZeroGX.iso',                            icon: 'GCN',  emulator: 'dolphin' },
-  // PS2 (PCSX2)
-  { id: 'ps2_GodOfWarII',       title: 'God of War II',               genre: 'PS2',      system: 'PlayStation 2',   status:'Perfect', profile: 'GodOfWarII.iso',                         icon: 'PS2',  emulator: 'pcsx2' },
-  { id: 'ps2_GT4',              title: 'Gran Turismo 4',              genre: 'Racing',   system: 'PlayStation 2',   status:'Perfect', profile: 'GranTurismo4.iso',                       icon: 'PS2',  emulator: 'pcsx2' },
-  { id: 'ps2_SoulCalibur3',     title: 'SoulCalibur III',             genre: 'Fighting', system: 'PlayStation 2',   status:'Great',   profile: 'SoulCaliburIII.iso',                     icon: 'PS2',  emulator: 'pcsx2' },
-  // MAME Arcade Classics
-  { id: 'mame_pacman',          title: 'Pac-Man',                    genre: 'Arcade',    system: 'MAME',           status: 'Perfect', profile: 'pacman.zip',              icon: 'M',  emulator: 'mame'       },
-  { id: 'mame_galaga',          title: 'Galaga',                     genre: 'Arcade',    system: 'MAME',           status: 'Perfect', profile: 'galaga.zip',              icon: 'M',  emulator: 'mame'       },
-  { id: 'mame_sf2',             title: 'Street Fighter II',          genre: 'Fighting',  system: 'MAME',           status: 'Perfect', profile: 'sf2.zip',                 icon: 'M',  emulator: 'mame'       },
-  { id: 'mame_mk',              title: 'Mortal Kombat',              genre: 'Fighting',  system: 'MAME',           status: 'Perfect', profile: 'mk.zip',                  icon: 'M',  emulator: 'mame'       },
-  { id: 'mame_dkong',           title: 'Donkey Kong',                genre: 'Arcade',    system: 'MAME',           status: 'Perfect', profile: 'dkong.zip',               icon: 'M',  emulator: 'mame'       },
-  // RetroArch / Classic Consoles
-  { id: 'ra_SuperMarioBros',    title: 'Super Mario Bros.',          genre: 'Retro',     system: 'NES',            status: 'Perfect', profile: 'SuperMarioBros.nes',      icon: 'R',  emulator: 'retroarch'  },
-  { id: 'ra_SuperMarioWorld',   title: 'Super Mario World',          genre: 'Retro',     system: 'SNES',           status: 'Perfect', profile: 'SuperMarioWorld.sfc',     icon: 'R',  emulator: 'retroarch'  },
-  { id: 'ra_SonicTheHedgehog',  title: 'Sonic the Hedgehog',         genre: 'Retro',     system: 'Genesis',        status: 'Perfect', profile: 'SonicTheHedgehog.md',     icon: 'R',  emulator: 'retroarch'  },
-  { id: 'ra_PokemonRed',        title: 'Pokemon Red',                genre: 'Retro',     system: 'Game Boy',       status: 'Perfect', profile: 'PokemonRed.gb',           icon: 'R',  emulator: 'retroarch'  },
-  { id: 'ra_MarioKart64',       title: 'Mario Kart 64',              genre: 'Racing',    system: 'N64',            status: 'Perfect', profile: 'MarioKart64.n64',         icon: 'R',  emulator: 'retroarch'  },
-  // Project64 / N64
-  { id: 'n64_Goldeneye007',     title: 'GoldenEye 007',              genre: 'N64',       system: 'Nintendo 64',    status: 'Perfect', profile: 'Goldeneye007.z64',        icon: '64', emulator: 'project64'  },
-  { id: 'n64_Zelda_OOT',        title: 'Zelda: Ocarina of Time',     genre: 'N64',       system: 'Nintendo 64',    status: 'Perfect', profile: 'ZeldaOcarinaOfTime.z64',  icon: '64', emulator: 'project64'  },
-  { id: 'n64_SuperSmashBros',   title: 'Super Smash Bros.',          genre: 'Fighting',  system: 'Nintendo 64',    status: 'Perfect', profile: 'SuperSmashBros.n64',      icon: '64', emulator: 'project64'  },
-  // DuckStation / PS1
-  { id: 'ps1_CrashBandicoot',   title: 'Crash Bandicoot',            genre: 'PS1',       system: 'PlayStation',    status: 'Perfect', profile: 'CrashBandicoot.bin',      icon: 'DS', emulator: 'duckstation' },
-  { id: 'ps1_FinalFantasyVII',  title: 'Final Fantasy VII',          genre: 'PS1',       system: 'PlayStation',    status: 'Perfect', profile: 'FinalFantasyVII.bin',     icon: 'DS', emulator: 'duckstation' },
-  { id: 'ps1_MetalGearSolid',   title: 'Metal Gear Solid',           genre: 'PS1',       system: 'PlayStation',    status: 'Perfect', profile: 'MetalGearSolid.bin',      icon: 'DS', emulator: 'duckstation' },
-  // Flycast / Dreamcast
-  { id: 'dc_SonicAdventure',    title: 'Sonic Adventure',            genre: 'Dreamcast', system: 'Dreamcast',      status: 'Perfect', profile: 'SonicAdventure.gdi',      icon: 'DC', emulator: 'flycast'    },
-  { id: 'dc_MarvelVsCapcom2',   title: 'Marvel vs. Capcom 2',        genre: 'Fighting',  system: 'Dreamcast',      status: 'Perfect', profile: 'MarvelVsCapcom2.gdi',     icon: 'DC', emulator: 'flycast'    },
-  { id: 'dc_CrazyTaxi',         title: 'Crazy Taxi',                 genre: 'Racing',    system: 'Dreamcast',      status: 'Perfect', profile: 'CrazyTaxi.gdi',           icon: 'DC', emulator: 'flycast'    },
-  // Sega Model 2
-  { id: 'model2_daytona',  title: 'Daytona USA',           genre: 'Model2',  system: 'Sega Model 2', status: 'Perfect', profile: 'daytona.zip',  icon: 'M2', emulator: 'model2' },
-  { id: 'model2_vf2',      title: 'Virtua Fighter 2',      genre: 'Fighting',system: 'Sega Model 2', status: 'Perfect', profile: 'vf2.zip',      icon: 'M2', emulator: 'model2' },
-  { id: 'model2_srallyc',  title: 'Sega Rally Championship',genre: 'Racing', system: 'Sega Model 2', status: 'Perfect', profile: 'srallyc.zip',  icon: 'M2', emulator: 'model2' },
-  // Sega Model 3
-  { id: 'model3_scud',     title: 'Scud Race / Super GT',  genre: 'Model3',  system: 'Sega Model 3', status: 'Perfect', profile: 'scud.zip',     icon: 'M3', emulator: 'model3' },
-  { id: 'model3_vf3',      title: 'Virtua Fighter 3',      genre: 'Fighting',system: 'Sega Model 3', status: 'Perfect', profile: 'vf3.zip',      icon: 'M3', emulator: 'model3' },
-  { id: 'model3_starwars', title: 'Star Wars Trilogy Arcade',genre: 'Shooter',system: 'Sega Model 3', status: 'Perfect', profile: 'starwars.zip', icon: 'M3', emulator: 'model3' },
-  // PPSSPP / PSP
-  { id: 'psp_GodOfWarChainsOfOlympus', title: 'God of War: Chains of Olympus', genre: 'PSP', system: 'PlayStation Portable', status: 'Perfect', profile: 'GodOfWarChainsOfOlympus.iso', icon: 'PSP', emulator: 'ppsspp' },
-  { id: 'psp_MonsterHunterFreedom',    title: 'Monster Hunter Freedom Unite',   genre: 'PSP', system: 'PlayStation Portable', status: 'Perfect', profile: 'MonsterHunterFreedomUnite.iso', icon: 'PSP', emulator: 'ppsspp' },
-  { id: 'psp_GrandTheftAutoSA',        title: 'Grand Theft Auto: San Andreas',  genre: 'PSP', system: 'PlayStation Portable', status: 'Perfect', profile: 'GTASanAndreas.iso', icon: 'PSP', emulator: 'ppsspp' },
-  // Cemu / Wii U
-  { id: 'wiiu_MarioKart8',   title: 'Mario Kart 8',                    genre: 'WiiU', system: 'Wii U', status: 'Perfect', profile: 'MarioKart8', icon: 'WU', emulator: 'cemu' },
-  { id: 'wiiu_ZeldaBOTW',    title: 'Zelda: Breath of the Wild',       genre: 'WiiU', system: 'Wii U', status: 'Perfect', profile: 'ZeldaBOTW',  icon: 'WU', emulator: 'cemu' },
-  { id: 'wiiu_Splatoon',     title: 'Splatoon',                        genre: 'WiiU', system: 'Wii U', status: 'Perfect', profile: 'Splatoon',   icon: 'WU', emulator: 'cemu' },
-  // Pinball
-  { id: 'Medieval_Madness',     title: 'Medieval Madness',            genre: 'Pinball',  system: 'Visual Pinball X',status:'Perfect', profile: 'Medieval_Madness.vpx',                   icon: '?',  emulator: 'vpx', isPinball: true },
-  { id: 'Attack_From_Mars',     title: 'Attack From Mars',            genre: 'Pinball',  system: 'Visual Pinball X',status:'Perfect', profile: 'Attack_From_Mars.vpx',                   icon: '?',  emulator: 'vpx', isPinball: true },
-  { id: 'The_Addams_Family',    title: 'The Addams Family',           genre: 'Pinball',  system: 'Visual Pinball X',status:'Perfect', profile: 'The_Addams_Family.vpx',                  icon: '?',  emulator: 'vpx', isPinball: true },
-]
+const SAMPLE_GAMES = []
 
 const FAVORITES_KEY = 'nuarcade_favorites'
 const RECENT_KEY    = 'nuarcade_recent'
@@ -364,7 +283,7 @@ export function useGameLibrary() {
             }
           } catch {}
 
-          const markedGames = (!libraryEmpty ? allGames : SAMPLE_GAMES).map((g, i) => {
+          const markedGames = allGames.map((g, i) => {
             const gameId = g.id || g.profile?.replace('.xml', '').replace('.vpx', '')
             const videoFilePath = videosMap[gameId]
             return {
