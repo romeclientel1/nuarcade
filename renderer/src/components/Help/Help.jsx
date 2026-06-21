69390de6ea39d01d3f9b44b3adf931c50dc86fe5
romeclientel1 <jerome_emanuel@yahoo.com>
2026-06-20 22:15:06 -0400

    Update Help.jsx

--- renderer/src/components/Help/Help.jsx
+++ renderer/src/components/Help/Help.jsx
@@ -1,5 +1,4 @@
 import { useRef } from 'react'
-import { useGamepad } from '../../hooks/useGamepad'
 import { useOverlayGamepad } from '../../hooks/useOverlayGamepad'
 import styles from './Help.module.css'
 
@@ -49,12 +48,6 @@ const EMULATORS = [
 ]
 
 export default function Help({ onClose }) {
-  const scrollRef = useRef(null)
-  useGamepad({
-    back: onClose,
-    up:   () => scrollRef.current?.scrollBy({ top: -120, behavior: 'smooth' }),
-    down: () => scrollRef.current?.scrollBy({ top:  120, behavior: 'smooth' }),
-  })
   const bodyRef = useRef(null)
   useOverlayGamepad({
     onClose,
