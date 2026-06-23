2dd5effe2961b372d979f91a56229bba1a927d19
romeclientel1 <jerome_emanuel@yahoo.com>
2026-06-23 13:04:26 -0400

    Update Wizard.jsx

--- renderer/src/components/Wizard/Wizard.jsx
+++ renderer/src/components/Wizard/Wizard.jsx
@@ -137,7 +137,7 @@ const handleExit = () => {
             </div>
           ))}
         </div>
-        <div className={styles.stepLabel}>Step {step + 1} of {SCREENS.length}</div>
+        <div className={styles.stepLabel}>step === 0 ? 'Welcome' : 'Step ' + step + ' of ' + (SCREENS.length - 1)</div>
       </div>
 
       <div className={styles.body}>
