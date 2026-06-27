4cc38825b9edaaffed80f4c27456759153b88380
romeclientel1 <jerome_emanuel@yahoo.com>
2026-06-27 09:15:42 -0400

    Update CURRENT_VERSION to 4.4.73

--- renderer/src/hooks/useVersionCheck.js
+++ renderer/src/hooks/useVersionCheck.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-const CURRENT_VERSION = "4.4.72"
+const CURRENT_VERSION = "4.4.73"
 const RELEASES_URL    = 'https://github.com/romeclientel1/nuarcade/releases'
 const GITHUB_API      = 'https://api.github.com/repos/romeclientel1/nuarcade/releases/latest'
 
