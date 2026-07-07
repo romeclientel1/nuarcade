ed0f528adc11b07bf7656afb682d7b7dbcf93002
romeclientel1 <jerome_emanuel@yahoo.com>
2026-07-07 18:36:23 -0400

    Update CURRENT_VERSION to 4.32.0

--- renderer/src/hooks/useVersionCheck.js
+++ renderer/src/hooks/useVersionCheck.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-const CURRENT_VERSION = "4.31.2"
+const CURRENT_VERSION = "4.32.0"
 
 export function useVersionCheck() {
   const [updateAvailable, setUpdateAvailable] = useState(false)
