93346c9db108d73e0931aa7148f00416b89614a3
romeclientel1 <jerome_emanuel@yahoo.com>
2026-07-10 08:58:51 -0400

    Add system parameter to launchRetroArchGame function

--- src/preload/index.js
+++ src/preload/index.js
@@ -33,7 +33,7 @@ contextBridge.exposeInMainWorld('nuarcade', {
   launchSwitchGame:    (gamePath)     => ipcRenderer.invoke('launch-switch-game', gamePath),
   launchMameGame:      (gamePath)     => ipcRenderer.invoke('launch-mame-game', gamePath),
   launchRetroArch:     ()             => ipcRenderer.invoke('launch-retroarch'),
-  launchRetroArchGame: (gamePath)     => ipcRenderer.invoke('launch-retroarch-game', gamePath),
+  launchRetroArchGame: (gamePath, system) => ipcRenderer.invoke('launch-retroarch-game', gamePath, system),
   launchN64Game:       (gamePath)     => ipcRenderer.invoke('launch-n64-game', gamePath),
   launchPs1Game:       (gamePath)     => ipcRenderer.invoke('launch-ps1-game', gamePath),
   launchFlycastGame:   (gamePath)     => ipcRenderer.invoke('launch-flycast-game', gamePath),
