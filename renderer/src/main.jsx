9724d603d8cbd95944cbeab485afa3d157bd7694
romeclientel1 <jerome_emanuel@yahoo.com>
2026-07-19 05:07:25 -0400

    Wrap App in ProfileProvider

--- renderer/src/main.jsx
+++ renderer/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { ProfileProvider } from './context/ProfileContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ProfileProvider>
+      <App />
+    </ProfileProvider>
   </StrictMode>,
 )
