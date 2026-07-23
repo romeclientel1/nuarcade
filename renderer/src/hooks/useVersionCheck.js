d5c9ec099ad4d7c24724306c3017501b09d9a4d0
Jerome Emanuel <jerome_emanuel@yahoo.com>
2026-07-23 17:56:36 -0400

    Bump version to 5.6.4

--- renderer/src/hooks/useVersionCheck.js
+++ renderer/src/hooks/useVersionCheck.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-const CURRENT_VERSION = "5.6.3"
+const CURRENT_VERSION = "5.6.4"
 
 export function useVersionCheck() {
   const [updateAvailable, setUpdateAvailable] = useState(false)
