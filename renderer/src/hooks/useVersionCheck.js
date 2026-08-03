610a14c20feb7cbf7dd44cf6765f2c3adfd36cba
Rome Clientel <romeclientel@gmail.com>
2026-08-03 09:00:28 -0400

    Bump Vespara version to 6.0.5

--- renderer/src/hooks/useVersionCheck.js
+++ renderer/src/hooks/useVersionCheck.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-const CURRENT_VERSION = "6.0.4"
+const CURRENT_VERSION = "6.0.5"
 
 export function useVersionCheck() {
   const [updateAvailable, setUpdateAvailable] = useState(false)
